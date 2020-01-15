7368de67888c41bee8dd7ac15f2a0d4896798a5d
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-14 23:11:51 -0300

    add react-redux-form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Footer from './FooterComponent'
 import {Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import {connect} from 'react-redux'
 import { addComment, fetchDishes } from '../redux/ActionCreators'
+import {actions} from 'react-redux-form'
 
 class Main extends React.Component {
 
@@ -65,8 +66,8 @@ class Main extends React.Component {
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>}/>
                 <Route path={'/menu/:dishId'} component={DishWithId}/>
-                <Route exact path='/contactus' component={Contact}/>
-                <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}/>} />
+                <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
+                <Route exact path='/ aboutus' component={() => <About leaders={this.props.leaders}/>} />
                 <Redirect to='/home'/>
             </Switch>
           <Footer/>
@@ -86,6 +87,7 @@ class Main extends React.Component {
 
  const mapDispatchToProps = dispatch => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-    fetchDishes: () => {dispatch(fetchDishes())}
+    fetchDishes: () => {dispatch(fetchDishes())},
+    resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
   })
  export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main)) 
