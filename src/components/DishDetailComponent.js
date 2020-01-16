cf5864d95dc86c975a1eee0b8b6aa72c43633ce9
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-15 21:44:55 -0300

    react animated components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 import {Control, LocalForm, Errors} from 'react-redux-form'
 import { Loading } from './LoadingComponent'
 import {baseUrl} from '../shared/baseUrl'
-
+import {FadeTransform, Fade, Stagger} from 'react-animation-components'
 
 
 const maxLength = len => val => !(val) || (val.length <= len) 
@@ -77,14 +77,18 @@ export default class DishDetail extends React.Component{
             return(
             <div>
             <ul className='list-unstyled'>
+                <Stagger in>
                 {comments.map(comment => { 
                     return(
-                        <li>
-                            <p>{comment.comment}</p>
-                            <p> --{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                        </li>
+                        <Fade in>
+                            <li>
+                                <p>{comment.comment}</p>
+                                <p> --{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        </Fade>
                     )
                     })}
+                 </Stagger>
             </ul>
             <CommentForm dishId={dishId} postComment={postComment}/>
             </div>
@@ -131,6 +135,8 @@ export default class DishDetail extends React.Component{
             </div>
             <div className='row'>
                 <div className='col-12 col-md-5 m-1'>
+                <FadeTransform in 
+                 transformProps={{exitTransform: 'scale(0.5) translate Y(-50%)'}}>
                     <Card>
                         <CardImg  object src={baseUrl + this.props.dish.image} alt={this.props.dish.name}/>
                          <CardBody>
@@ -138,6 +144,7 @@ export default class DishDetail extends React.Component{
                             <CardText>{this.props.dish.description}</CardText>
                         </CardBody>
                     </Card>
+                 </FadeTransform>
                 </div>
                 <div className='col-12 col-md-5 m-1'>
                     <h4>Comments</h4>
